afd16d6ff053bd8017ae215b2a2968aad2ba04ce
osekmedia <osekmedia@gmail.com>
2018-06-27 20:20:48 -0700

    Initial commit of repo

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//Register Partials dir
+hbs.registerPartials(__dirname + '/views/partials');
+//set handlebars view engine using hbs => https://www.npmjs.com/package/hbs
+app.set('view engine', 'hbs');
+
+//Custom middleware
+app.use( (req, res, next)=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log( log );
+	fs.appendFile('server.log', log + '\n', (err)=>{
+		if(err){
+			console.log('Unable to write to server log');
+		}
+	} );
+	next(); //App winn not run past middleware until next()
+});
+
+// app.use( (req, res, next)=>{
+// 	res.render('maintenance.hbs');
+// });
+
+//middleware
+app.use( express.static(__dirname + '/public') ); 
+
+//register helpers
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',( text )=>{
+	return text.toUpperCase();
+})
+
+app.get('/', ( req, res )=>{
+	//res.send({name: 'Erin', likes: ['Amy', 'Penelope']});
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my node website!!'
+	});
+});
+
+app.get('/about',(req,res)=>{
+	//res.send('About Page!!');
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+})
+
+app.get('/bad', ( req, res )=>{
+	res.send({errorMessage: 'Unable to process request'});
+});
+
+app.listen(3000, () => {
+	console.log('Server running on port 3000');
+});
